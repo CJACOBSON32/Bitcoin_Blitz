04abf92a059f01495c32a3cb6c7c7becc9bee96b
Kenneth Nguyen <knguyen3159@gmail.com>
2022-10-05 14:14:24 -0400

    Adjusted main view and got Main button to work. Needs to get rid of borders

--- app/src/main/java/info/cs4518/bitcoinblitz/MainActivity.kt
+++ app/src/main/java/info/cs4518/bitcoinblitz/MainActivity.kt
@@ -3,15 +3,19 @@ package info.cs4518.bitcoinblitz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.ViewModelProvider
 
 
 class MainActivity : AppCompatActivity() {
 
 	private val overclockMeter = 0
+	private val wallet = 0
 
-	private lateinit var clickCounterTextview: TextView
+	private lateinit var mainView: ConstraintLayout
+	//private lateinit var clickCounterTextview: TextView
 	private lateinit var bitcoinPerSecondTextview: TextView
 	private lateinit var walletTextView: TextView
 
@@ -23,19 +27,22 @@ class MainActivity : AppCompatActivity() {
 		setContentView(R.layout.activity_main)
 
 //		clickCounterTextview = findViewById(R.id.player_click_counter)
-//		bitcoinPerSecondTextview = findViewById(R.id.bitcoin_per_second)
-//		walletTextView = findViewById(R.id.wallet)
+		mainView = findViewById(R.id.main_view)
+		bitcoinPerSecondTextview = findViewById(R.id.bitcoin_per_second_view)
+		walletTextView = findViewById(R.id.wallet_view)
 
 		viewModel = ViewModelProvider(this)[PlayerViewModel::class.java]
 
-		clickCounterTextview.text = viewModel.clickCounter.toString()
+		//clickCounterTextview.text = viewModel.clickCounter.toString()
 		bitcoinPerSecondTextview.text = viewModel.bitcoinPerSecond.toString()
 		walletTextView.text = viewModel.wallet.toString()
 	}
 
+/*
 	override fun onDestroy() {
 		super.onDestroy()
 	}
+*/
 
 	fun tapBitcoin(view: View?){
 		viewModel.wallet += viewModel.clickPotency
@@ -46,5 +53,4 @@ class MainActivity : AppCompatActivity() {
 	fun calculateBPS(view: View?){
 
 	}
-
 }
