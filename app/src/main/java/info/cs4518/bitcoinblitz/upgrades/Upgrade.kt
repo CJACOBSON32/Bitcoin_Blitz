14381707691e9d0a4c214a662c8ea52fe0155e86
Kenneth Nguyen <knguyen3159@gmail.com>
2022-10-13 11:26:16 -0400

    added firebase startup and initialized all upgrades

--- app/src/main/java/info/cs4518/bitcoinblitz/upgrades/Upgrade.kt
+++ app/src/main/java/info/cs4518/bitcoinblitz/upgrades/Upgrade.kt
@@ -1,5 +1,9 @@
 package info.cs4518.bitcoinblitz.upgrades
 
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 abstract class Upgrade(
 	val id: Int? = null,
 	val name: String? = null,
@@ -7,3 +11,4 @@ abstract class Upgrade(
 	val cost: Float? = null,
 	val description: String? = null
 )
+
